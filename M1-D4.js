06915d759b52444521fa04f1c14d2ad43fb2ca55
Hung Jin Chong <hungjinchong@Outlook.com>
2020-10-01 13:41:04 +0100

    Question 2
    Done question 2

--- M1-D4.js
+++ M1-D4.js
@@ -21,6 +21,11 @@ console.log(
 /* EXERCISE 2
 Write a function "crazySum" which receives two given integers. If the two values are same, then returns triple their sum.
 */
+console.log("\nExercise 2");
+let crazySum = (no1, no2) => (no1 === no2 ? (no1 + no2) * 3 : false);
+no1 = 2;
+no2 = 3;
+console.log(`Function crazySum returns ${crazySum(no1, no2)}`);
 
 /* EXERCISE 3
 Write a function "crazyDiff" that computes the 
