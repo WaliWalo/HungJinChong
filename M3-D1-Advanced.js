306146f76244022f8e0298fbc6f201c97bffe7e1
Hung Jin Chong <hungjinchong@Outlook.com>
2020-11-02 15:31:58 +0000

    done up to ex5 adv

--- M3-D1-Advanced.js
+++ M3-D1-Advanced.js
@@ -1,3 +1,5 @@
+const { resetWarningCache, string } = require("prop-types");
+
 /* 1) MAX CHAR
 
 Given a string, return the character that is most
@@ -7,7 +9,30 @@ commonly used in the string.
     maxChar("abcccccccd") === "c"
     maxChar("apple 1231111") === "1"
 */
+console.log("\nExercise 1");
+const ex1 = function (str) {
+  let split = str.toLowerCase().split("");
+  let char = {};
+  for (let i = 0; i < split.length; i++) {
+    if (!char[split[i]]) {
+      char[split[i]] = 1;
+    } else {
+      char[split[i]]++;
+    }
+  }
 
+  let max = 0;
+  let maxChar = "";
+  for (let i in char) {
+    console.log(i, char[i]);
+    if (char[i] > max) {
+      max = char[i];
+      maxChar = i;
+    }
+  }
+  return "Most common used Character is " + maxChar;
+};
+console.log(ex1("apple 1231111"));
 /* 2) ANAGRAMS
 
 Check to see if two provided strings are anagrams of each other.
@@ -20,7 +45,23 @@ or punctuation.  Consider capital letters to be the same as lower case
   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
   anagrams('Hi there', 'Bye there') --> False
 */
-
+console.log("\nExercise 2");
+const ex2 = function (str1, str2) {
+  str1 = str1.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
+  str2 = str2.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
+  let split1 = str1.toLowerCase().split("");
+  let split2 = str2.toLowerCase().split("");
+  split1.sort();
+  split2.sort();
+  let result = true;
+  for (let i = 0; i < split1.length; i++) {
+    if (split1[i] !== split2[i]) {
+      result = false;
+    }
+  }
+  return result;
+};
+console.log(ex2("rail safety", "fairy tales"));
 /* 3) ANAGRAMS 2
 
 Given a word and a list of possible anagrams, select the correct sublist.
@@ -29,6 +70,36 @@ Given a word and a list of possible anagrams, select the correct sublist.
 
     "listen" and a list of candidates like "enlists" "google" "inlets" "banana" the program should return a list containing "inlets".
 */
+console.log("\nExercise 2");
+const ex3 = function (str, arr) {
+  str = str
+    .replace(/[^\w\s]|_/g, "")
+    .replace(/\s+/g, "")
+    .toLowerCase();
+  sortedStr = str.split("").sort();
+  let strings = [];
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i]
+      .replace(/[^\w\s]|_/g, "")
+      .replace(/\s+/g, "")
+      .toLowerCase();
+    strings.push(arr[i].split("").sort());
+  }
+  let result = true;
+  let texts = "";
+  for (let i = 0; i < strings.length; i++) {
+    for (let j = 0; j < str.length; j++) {
+      if (sortedStr[j] !== strings[i][j]) {
+        result = false;
+      } else {
+        result = true;
+      }
+    }
+    texts = texts + `${arr[i]} and ${str} is an anagram: ${result}` + "\n";
+  }
+  return texts;
+};
+console.log(ex3("listen", ["enlist", "google", "inlets", "banana"]));
 
 /* 4) PALINDROME
 
@@ -41,7 +112,29 @@ and punctuation in determining if the string is a palindrome.
 
     palindrome("abba") === true
     palindrome("abcdefg") === false
+    split.length must be even
  */
+console.log("\nExercise 4");
+const ex4 = function (str) {
+  let split = str.split("");
+  let result = false;
+  if (split.length % 2 !== 0) {
+    result = false;
+  }
+  let part1;
+  let part2;
+  for (let i = 0; i < split.length / 2; i++) {
+    part1 = part1 + split[i];
+  }
+  for (let i = split.length - 1; i >= split.length / 2; i--) {
+    part2 = part2 + split[i];
+  }
+  if (part1 === part2) {
+    result = true;
+  }
+  return `${str} is a palindrom: ${result}`;
+};
+console.log(ex4("aabbaa"));
 
 /* 5) REVERSE INT
 
@@ -56,6 +149,22 @@ ordering of numbers.
     reverseInt(-15) === -51
     reverseInt(-90) === -9
  */
+console.log("\nExercise 5");
+const ex5 = function (x) {
+  x = x.toString().replace("0", "");
+  let split = x.toString().split("");
+  let xtra = "";
+  if (Number.isInteger(parseInt(split[0])) === false) {
+    xtra = split[0];
+    split = split.slice(1);
+  }
+  let result = "";
+  for (let i = split.length - 1; i >= 0; i--) {
+    result = result + split[i];
+  }
+  return xtra + result;
+};
+console.log(ex5(-90));
 
 /* 6) STEPS
 
